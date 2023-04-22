afa54cba2ef611b2a1cd3ebeee671fd01aff4567
AmauryD <deflorenne.amaury@gmail.com>
2023-04-22 22:30:00 +0200

    rm dist

--- dist/a.js
+++
@@ -1,8 +0,0 @@
-// a function seems necessary to hang the server
-function somethingReturningFindOptions() {
-    return {};
-}
-class UserModel {
-}
-({}.findOne('one', somethingReturningFindOptions()));
-export {};
